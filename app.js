b705492a0e257653b29db42a8843866eb2626cce
Vid Ribic <ribvid@gmail.com>
2019-11-28 14:15:08 +0100

    endpoint for mejnik1

--- app.js
+++ app.js
@@ -14,4 +14,15 @@ app.use(express.json())
 const eventsRouter = require('./routes/events')
 app.use('/events', eventsRouter)
 
+app.get('/mejnik', async (req, res) => {
+	res.status(200).json({
+		"clani": ["vr9223"],
+		"opis_projekta": "Moj projekt implementira aplikacijo za dodajanje dogodkov, dvoran in prodajo vstopnic.",
+		"mikrostoritve": ["https://rso-events.azurewebsites.net/events"],
+		"github": ["https://github.com/rso-event-manager/events"],
+		"travis": ["https://travis-ci.org/rso-event-manager/events"],
+		"dockerhub": ["https://hub.docker.com/r/ribvid/rso-events"]
+	})
+})
+
 app.listen(port, () => console.log(`Server started`))
