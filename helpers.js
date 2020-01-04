985051abe8793c7f78c28635d9f8036a7c4e90f3
Vid Ribic <ribvid@gmail.com>
2020-01-04 17:17:36 +0100

    removed healthwatchers

--- helpers.js
+++ helpers.js
@@ -8,47 +8,6 @@ const lightship = createLightship({
 	detectKubernetes: true
 })
 
-const healthWatcher = consul.watch({
-	method: consul.health.service,
-	options: {
-		service: 'events',
-	}
-});
-
-healthWatcher.on('change', data => {
-	for (let el of data) {
-		consul.agent.check.register({
-			name: 'health',
-			id: 'health',
-			serviceid: el.Service.ID,
-			http: 'http://localhost:9000/health',
-			interval: '10s',
-			timeout: '1s',
-		}, function (err) {
-			if (err) {
-				console.log(err.message)
-			}
-		});
-	}
-});
-
-// consul.agent.service.list(function(err, result) {
-// 	if (err) throw err;
-// 	console.log(result)
-// });
-
-// consul.agent.check.register({
-// 	'name': 'Check events health',
-// 	'http': 'http://localhost:9000/health',
-// 	'interval': '10s',
-// 	'timeout': '1s',
-// 	'serviceid': 'events'
-// }, function (err) {
-// 	if (err) {
-// 		console.log('err', err.message)
-// 	}
-// })
-
 module.exports = {
 	consul: consul,
 	lightship: lightship,
