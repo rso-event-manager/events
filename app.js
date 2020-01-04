575fd949ef62ef3e1d6784531ac82fcba3b569cf
Vid Ribic <ribvid@gmail.com>
2020-01-05 00:36:38 +0100

    winston

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const mongoose = require('mongoose');
-const { consul, lightship } = require('./helpers')
+const { consul, lightship, winston } = require('./helpers')
 
 const watcher = consul.watch({
 	method: consul.kv.get,
@@ -12,16 +12,19 @@ const watcher = consul.watch({
 watcher.on('change', data => {
 	mongoose.connect(data.Value, {useNewUrlParser: true, useUnifiedTopology: true}).catch(err => {
 		console.error(`Mongoose has failed. ${err.message}`)
+		winston.error(`Mongoose has failed. ${err.message}`)
 		lightship.signalNotReady()
 	})
 
 	const db = mongoose.connection
 	db.on('error', (error) => {
 		console.error(`Cannot connect to db ${data.Value}. ${error.message}`)
+		winston.error(`Cannot connect to db ${data.Value}. ${error.message}`)
 		lightship.signalNotReady()
 	})
 	db.once('open', (error) => {
 		console.log('Connected to db')
+		winston.info("Connected to db");
 		lightship.signalReady()
 	})
 })
@@ -38,6 +41,7 @@ app.use('/unhealthy', (req, res) => {
 
 const server = app.listen(port, () => {
 	console.log(`Server started`)
+	winston.info("Server started");
 	lightship.signalReady()
 })
 
--- helpers.js
+++ helpers.js
@@ -3,10 +3,21 @@ const consul = require('consul')({
 	port: 8500
 })
 
-const { createLightship } = require('lightship')
+const {createLightship} = require('lightship')
 const lightship = createLightship()
+
+const winston = require('winston');
+require('winston-logstash');
+
+winston.add(winston.transports.Logstash, {
+	port: 11667,
+	host: '3f5a38c5-2098-4987-b32e-7f9815e9aaaf-ls.logit.io',
+	ssl_enable: true,
+	max_connect_retries: -1,
+});
 
 module.exports = {
 	consul: consul,
 	lightship: lightship,
+	winston: winston,
 }
