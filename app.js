59abb0b25b0e9dd5897dec7ce3b384567d9aa207
Vid Ribic <ribvid@gmail.com>
2020-01-04 11:49:21 +0100

    removed mejnik endpoint

--- app.js
+++ app.js
@@ -18,15 +18,4 @@ app.get('/', async (req, res) => {
 	res.send('Hello World!')
 })
 
-app.get('/mejnik', async (req, res) => {
-	res.status(200).json({
-		"clani": ["vr9223"],
-		"opis_projekta": "Moj projekt implementira aplikacijo za dodajanje dogodkov, dvoran in prodajo vstopnic.",
-		"mikrostoritve": ["https://rso-events.azurewebsites.net/events"],
-		"github": ["https://github.com/rso-event-manager/events"],
-		"travis": ["https://travis-ci.org/rso-event-manager/events"],
-		"dockerhub": ["https://hub.docker.com/r/ribvid/rso-events"]
-	})
-})
-
 app.listen(port, () => console.log(`Server started`))
